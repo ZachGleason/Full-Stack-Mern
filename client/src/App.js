6a54055dd11633a67f23b9fe909156f2e4c41022
Zach <zgleason37@gmail.com>
2022-04-20 14:40:34 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PersonForm from './components/PersonForm';
+import './App.css';
+
+function App() {
+  return (
+    <PersonForm />
+  );
+}
+
+export default App;
